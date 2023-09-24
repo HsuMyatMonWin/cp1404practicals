5fa4665b3a59b077f26429f3abb92ada3a886ab5
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-09-24 19:32:44 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,22 @@
 MINIMUM_LENGTH = 8
 
-password = input("Enter password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f"Password must be {MINIMUM_LENGTH} characters long.")
-    password = input("Enter password: ")
 
-print("Password set.")
-print("*" * len(password))
+def main():
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password):
+    print("Password set.")
+    print("*" * len(password))
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password must be {MINIMUM_LENGTH} characters long.")
+        password = input("Enter password: ")
+    return password
+
+
+main()
