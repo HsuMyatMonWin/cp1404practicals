d074ddd789052f0e5e0dd7cb4bc24587145368d4
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-09-29 22:52:57 +1000

    Add random number file

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+# What did you see on line 1?
+# 19, 8, 10, 18, 10
+# What was the smallest number you could have seen, what was the largest?
+# Smallest - 5, Largest - 19
+#
+# What did you see on line 2?
+# 5, 7, 7, 3, 3
+# What was the smallest number you could have seen, what was the largest?
+# Smallest - 3, Largest - 9
+# Could line 2 have produced a 4?
+# No.
+#
+# What did you see on line 3?
+# 4.841527206562993, 4.403409662011951, 4.953157413779016, 3.2879470879240795, 5.461768190556595
+# What was the smallest number you could have seen, what was the largest?
+# Smallest - 2.5, Largest - 5.5
+#
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+print(random.randrange(1, 101))
+
