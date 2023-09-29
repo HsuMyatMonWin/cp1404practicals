7936917a7ca1ce083aa8b3ac89ed4fb7b5227eb1
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-09-30 00:36:27 +1000

    Add file closing statements to file manipulation

--- prac_03/files.py
+++ prac_03/files.py
@@ -18,6 +18,7 @@ in_file = open(file_name, "r")
 number_1 = int(in_file.readline().strip())
 number_2 = int(in_file.readline().strip())
 print(f"Result: {number_1 + number_2}")
+in_file.close()
 
 # Program 4
 file_name = "numbers.txt"
@@ -26,3 +27,4 @@ in_file = open(file_name, "r")
 for line in in_file:
     total += int(line.strip())
 print(f"Total: {total}")
+in_file.close()
