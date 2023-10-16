355e0b899d956fdf5738a07794b85003830e1945
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-10-16 10:48:58 +1000

    Add doc string to functions, refactor function names, change file_name to global constant

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -7,22 +7,26 @@ Actual:   48 minutes
 import csv
 
 
+FILE_NAME = "wimbledon.csv"
+
+
 def main():
-    file_name = "wimbledon.csv"
-    wimbledon_champions = load_data(file_name)
-    display_champions_and_winning_counts(wimbledon_champions)
+    wimbledon_champions = load_data(FILE_NAME)
+    display_champions(wimbledon_champions)
     print("")
     display_winning_countries(wimbledon_champions)
 
 
 def display_winning_countries(wimbledon_champions):
+    """ Display the name of countries that have won in Wimbledon. """
     winning_countries = set([country[3] for country in wimbledon_champions])
 
     print(f"These {len(winning_countries)} countries have won Wimbledon:")
     print(", ".join(sorted(winning_countries)))
 
 
-def display_champions_and_winning_counts(wimbledon_champions):
+def display_champions(wimbledon_champions):
+    """ Display champion names and their winning counts. """
     champion_to_winning_count = {}
     for record in wimbledon_champions:
         if record[2] in champion_to_winning_count:
@@ -35,6 +39,7 @@ def display_champions_and_winning_counts(wimbledon_champions):
 
 
 def load_data(file_name):
+    """ Read data from the csv file. """
     wimbledon_champions = []
     with open(file_name, "r", encoding="utf-8-sig") as in_file:
         reader = csv.reader(in_file, delimiter=",")
