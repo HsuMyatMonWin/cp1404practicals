4652efad45d05a3d381d3242319abc4a40fc4e1e
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-11-03 16:00:56 +1000

    Complete update project and add project for project management program exercise

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -19,6 +19,10 @@ def main():
     projects = load_projects(FILE_NAME)
     display_projects(projects)
     filter_project(projects)
+    update_project(projects)
+    display_projects(projects)
+    add_project(projects)
+    display_projects(projects)
 
 
 def load_projects(file_name):
@@ -74,6 +78,43 @@ def filter_project(projects):
     for project in projects:
         if project.start_date > date:
             print(project)
+
+
+def update_project(projects):
+    """
+    Display the project list, ask the user to choose a project, and then ask for new completion % and priority.
+    Update the project details with these new values. Retain existing values when input is blank.
+
+    :param projects: list, a list of projects.
+    :return: None
+    """
+    for i, project in enumerate(projects, 0):
+        print(f"{i} {project}")
+    choice = int(input("Project choice: "))
+    print(projects[choice])
+    new_percentage = float(input("New Percentage: "))
+    new_priority = int(input("New Priority:"))
+    if new_percentage != "":
+        projects[choice].completion_percentage = new_percentage
+    if new_priority != "":
+        projects[choice].priority = new_priority
+
+
+def add_project(projects):
+    """
+    Ask the name, start date, priority, cost estimate and completion percentage of a new project from a user
+    and add a new project to the project list.
+
+    :param projects: list, a list of projects.
+    :return: None
+    """
+    print("Let's add a new project")
+    name = input("Name: ")
+    start_date = input("Start date (dd/mm/yy): ")
+    priority = int(input("Priority: "))
+    cost_estimate = float(input("Cost estimate: "))
+    completion_percentage = float(input("Percent complete: "))
+    projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
 
 
 if __name__ == '__main__':
