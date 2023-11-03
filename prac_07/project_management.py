c2bcdf75f3145f6c9e4366a777b5255bed4adc80
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-11-03 16:32:20 +1000

    Complete save projects for project management program exercise

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,7 +4,7 @@ CP1404 Practical 07 Exercise program file
 
 Estimated Time: 1hour and 30mins
 Start Time: 2:55pm
-
+Break Time: 4:00pm - 4:25pm
 
 """
 from project import Project
@@ -14,15 +14,17 @@ FILE_NAME = "projects.txt"
 
 def main():
     """
-    doc string here
+    Load a list of projects from a file. Display a menu to the user and allow the user to add, update, display
+    and filter projects. Save the project list to a file when the user exit the program.
     """
     projects = load_projects(FILE_NAME)
     display_projects(projects)
-    filter_project(projects)
-    update_project(projects)
-    display_projects(projects)
+    # filter_project(projects)
+    # update_project(projects)
+    # display_projects(projects)
     add_project(projects)
-    display_projects(projects)
+    # display_projects(projects)
+    save_projects(projects, FILE_NAME)
 
 
 def load_projects(file_name):
@@ -115,6 +117,20 @@ def add_project(projects):
     cost_estimate = float(input("Cost estimate: "))
     completion_percentage = float(input("Percent complete: "))
     projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+
+
+def save_projects(projects, file_name):
+    """
+    Write the project list in the specified file.
+
+    :param projects:
+    :param file_name:
+    :return: None
+    """
+    with open(file_name, 'w', newline='') as out_file:
+        for project in projects:
+            print(project.name, project.start_date, project.priority, project.cost_estimate,
+                  project.completion_percentage, sep="\t", file=out_file)
 
 
 if __name__ == '__main__':
