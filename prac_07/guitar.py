bf58edd4e1f1c6fdeacf82fd11a2dc99c94ddcfb
Hsu Myat Mon Win <hsumyatmonw@gmail.com>
2023-11-03 14:04:45 +1000

    Fix code formatting

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -12,7 +12,6 @@ class Guitar:
     def __init__(self, name="", year=0, cost=0):
         """
         Initialise a Guitar instance.
-
         :param name: string, the name of the guitar, default = ""
         :param year: integer, the year the guitar was made, default = 0
         :param cost: float, the price of the guitar, default = 0
@@ -24,7 +23,6 @@ class Guitar:
     def __str__(self):
         """
         Return the name, year and cost of the guitar in a sentence.
-
         :return: str
         """
         return f"{self.name} ({self.year}) : ${self.cost:.2f}"
@@ -32,9 +30,7 @@ class Guitar:
     def __lt__(self, other):
         """
         Compare two Guitar instances by year.
-
         :param other: Guitar object, another guitar to compare with.
-
         :return: bool
         """
         return self.year < other.year
@@ -42,7 +38,6 @@ class Guitar:
     def get_age(self, current_year):
         """
         Return how old the guitar is in years.
-
         :param current_year: integer, the current year
         :return: int
         """
@@ -51,7 +46,6 @@ class Guitar:
     def is_vintage(self, current_year):
         """
         Determine whether the guitar is vintage or not.
-
         :param current_year: integer, the current year
         :return: bool
         """
